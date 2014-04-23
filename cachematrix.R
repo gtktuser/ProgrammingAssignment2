9c5034357c095862a3b71f927038058db410dc21
gtktuser <bogsinparis@gmail.com>
2014-04-23 23:03:30 +0300

    Update cachematrix.R
    added final comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,9 @@
-## Write a short comment describing this function
+## prepares a matrix for inverse caching
+## usage:
+## a <- makeCacheMatrix(matrix(1:4,2))
+## a$get() displays current values for a
+## a$set(matrix(1:9,3)) changes the matrix values for a
+## a$getInv() gets the inverse ONLY after cacheSolve(a) was called
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -14,6 +19,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## Return a matrix that is the inverse of 'x'
+## after it was previously "prepared" with makeCacheMatrix above
+## usage:
+## x <- makeCacheMatrix(matrix(1:4,2))
+## cacheSolve(x)
+
 cacheSolve <- function(x, ...) {
         m <- x$getInv()
         if(!is.null(m)) {
